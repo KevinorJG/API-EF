c1f17ca755209b414c5ee26f824a8d23b0c2f7fd
Kevin-Mendoza07 <kevingfx71@gmail.com>
2022-06-08 08:33:13 -0600

    Api xd

---
+++ API/Controllers/LibrosController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class LibrosController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            List<ViewModels.LibrosViewModel> list = new List<ViewModels.LibrosViewModel>();
+            using (Models.LibreriaEntities db = new Models.LibreriaEntities())
+            {
+                list = (from l in db.Libros
+                        select new ViewModels.LibrosViewModel
+                        {
+                            Id = l.Id,
+                            ISBN = l.ISBN,
+                            Titulo = l.Titulo,
+                            Autor = l.Autor,
+                            Temas = l.Temas,
+                            Editorial = l.Editorial
+                        }).ToList(); 
+            }
+            return Ok(list);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            ViewModels.LibrosViewModel libro = null;
+            using (Models.LibreriaEntities db = new Models.LibreriaEntities())
+            {
+                libro = db.Libros.Where(x => x.Id == id)
+                    .Select(x => new ViewModels.LibrosViewModel()
+                    {
+                        Id = x.Id,
+                        ISBN = x.ISBN,
+                        Titulo = x.Titulo,
+                        Autor = x.Autor,
+                        Temas = x.Temas,
+                        Editorial = x.Editorial
+                    }).FirstOrDefault<ViewModels.LibrosViewModel>();
+            }
+            if (libro == null)
+                return NotFound();
+            return Ok(libro);
+        }
+        [HttpPost]
+        public IHttpActionResult Post(ViewModels.LibrosViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("No es un modelo válido");
+            }
+                
+            using (Models.LibreriaEntities db = new Models.LibreriaEntities())
+            {
+                var oLibro = new Models.Libros();
+                oLibro.ISBN = model.ISBN;
+                oLibro.Titulo = model.Titulo;
+                oLibro.Autor = model.Autor;
+                oLibro.Temas = model.Temas;
+                oLibro.Editorial = model.Editorial;
+                db.Libros.Add(oLibro);
+                db.SaveChanges();
+            }
+            return Ok("Registro agregado correctamente");
+        }
+        [HttpPut]
+        public IHttpActionResult Put(ViewModels.LibrosViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("No es un modelo válido");
+            }
+            using(Models.LibreriaEntities db = new Models.LibreriaEntities())
+            {
+                var olibro = db.Libros.Where(x => x.Id.Equals(model.Id))
+                    .FirstOrDefault<Models.Libros>();
+                if(olibro != null)
+                {
+                    var oLibro = new Models.Libros();
+                    oLibro.ISBN = model.ISBN;
+                    oLibro.Titulo = model.Titulo;
+                    oLibro.Autor = model.Autor;
+                    oLibro.Temas = model.Temas;
+                    oLibro.Editorial = model.Editorial;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+                return Ok();
+            }
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("No es un id de libro válido");
+            }
+            using (Models.LibreriaEntities db = new Models.LibreriaEntities())
+            {
+                var libro = db.Libros.Where(x => x.Id.Equals(id))
+                    .FirstOrDefault();
+                db.Entry(libro).State = System.Data.Entity.EntityState.Deleted;
+                db.SaveChanges();
+            }
+            return Ok();
+        }
+    }
+}
---
+++ API_View/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using API;
+using Newtonsoft.Json;
+using ViewModels;
+
+namespace API_View
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetAll();
+        }
+        private async void GetAll()
+        {
+            using(var client = new HttpClient())
+            {
+                using(var response = await client.GetAsync("https://localhost:44329/api/Libros"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var LibrosJsonString = await response.Content.ReadAsStringAsync();
+                        dataGridView1.DataSource = JsonConvert.DeserializeObject<List<LibrosViewModel>>(LibrosJsonString)
+                            .ToList();
+                    }
+                    else
+                    {
+                        MessageBox.Show("cagaste, tu api no sirve");
+                    }
+                }
+            }
+        }
+    }
+}
